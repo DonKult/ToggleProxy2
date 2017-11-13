a7e75ac7889c4f6685aa049dae0ab870aa544f18
David Kalnischkies <david@kalnischkies.de>
2017-11-13 22:32:24 +0100

    move exception hostnames to arrays
    This should really be configureable in the options, but that involves a
    lot of message passing so I will leave that for later.

--- proxy.js
+++ proxy.js
@@ -10,8 +10,20 @@ browser.runtime.onMessage.addListener(msg => {
 
 browser.runtime.sendMessage({cmd: "init"});
 
+const exactHostnames = [
+	'localhost', '127.0.0.1', '0.0.0.0',
+	'fritz.box', 'openwrt', 'lede',
+];
+const startsWithHostnames = [
+	'192.168.1',
+];
+const endsWithHostnames = [
+	'.lan',
+];
+
 function FindProxyForURL(url, host) {
-	if (host === 'localhost' || host === '127.0.0.1' || host.endsWith('.lan'))
+	if (exactHostnames.includes(host) || startsWithHostnames.some(h => host.startsWith(h)) ||
+		endsWithHostnames.some(h => host.endsWith(h)))
 		return [{ type: 'direct', host: '', port: '' }];
 	if (proxyurl === undefined)
 		return "PROXY something-went-really-wrong:2903";
